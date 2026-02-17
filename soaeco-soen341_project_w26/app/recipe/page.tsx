ff468965b243145d400b0240190ea820c4e02541
youssefjedidi <youssefjedidi2005@gmail.com>
2026-02-17 13:27:25 -0500

    add recipe title

--- soaeco-soen341_project_w26/app/recipe/page.tsx
+++ soaeco-soen341_project_w26/app/recipe/page.tsx
@@ -1,102 +1,109 @@
-'use client';
+"use client";
 
-import { use, useEffect, useState } from 'react';
-import {formStyles, layoutStyles} from '@/lib/styles';
+import { use, useEffect, useState } from "react";
+import { formStyles, layoutStyles } from "@/lib/styles";
 
 export default function RecipePage() {
+  const [prepTime, setPrepTime] = useState("");
+  const [recipeTitle, setRecipeTitle] = useState("");
+  const [ingeredientInput, setIngredientInput] = useState("");
+  const [ingredients, setIngredients] = useState<string[]>([]);
+  const [costMode, setCostMode] = useState("");
+  const [cost, setCost] = useState("");
+  const [prepSteps, setPrepSteps] = useState("");
+  const [difficulty, setDifficulty] = useState(3);
 
-const [prepTime, setPrepTime] = useState('');
-const [ingeredientInput, setIngredientInput] = useState('');
-const [ingredients, setIngredients] = useState<string[]>([]);
-const [costMode, setCostMode] = useState('');
-const [cost, setCost] = useState('');
-const [prepSteps, setPrepSteps] = useState('');
-const [difficulty, setDifficulty] = useState(3);
-
-const addIngredient = () => {
-    if (ingeredientInput.trim() === '') return;
+  const addIngredient = () => {
+    if (ingeredientInput.trim() === "") return;
     setIngredients([...ingredients, ingeredientInput]);
-    setIngredientInput('');
-};
+    setIngredientInput("");
+  };
 
-const removeIngredient = (idx: number) => {
+  const removeIngredient = (idx: number) => {
     setIngredients(ingredients.filter((_, i) => i !== idx));
-};
+  };
 
-return (
+  return (
     <div className={layoutStyles.pageContainer}>
-        <div className={layoutStyles.formCard}>
-            <h1 className={layoutStyles.pageTitle}>Create Recipe</h1>
-
+      <div className={layoutStyles.formCard}>
+        <h1 className={layoutStyles.pageTitle}>Create Recipe</h1>
 
         <form>
-            {/* Prep Time */}
-            <div className="mb-4">
-                <label className={formStyles.label}>Prep Time (minutes)</label>
-                <input 
-                    className={formStyles.input}
-                    type="number" 
-                    min="0"
-                    value={prepTime}
-                    onChange={(e) => setPrepTime(e.target.value)} 
-                />
+          {/* recipie title */}
+          <div className="mb-4">
+            <label className={formStyles.label}> Recipe Title </label>
+            <input
+              className={formStyles.input}
+              type="text"
+              value={recipeTitle}
+              onChange={(e) => setRecipeTitle(e.target.value)}
+            />
+          </div>
+          {/* Prep Time */}
+          <div className="mb-4">
+            <label className={formStyles.label}>Prep Time (minutes)</label>
+            <input
+              className={formStyles.input}
+              type="number"
+              min="0"
+              value={prepTime}
+              onChange={(e) => setPrepTime(e.target.value)}
+            />
+          </div>
+
+          {/* Ingredients */}
+          <div className="mb-4">
+            <label className={formStyles.label}>Ingredients</label>
+
+            <div className="flex gap-2">
+              <input
+                className={formStyles.input}
+                type="text"
+                value={ingeredientInput}
+                onChange={(e) => setIngredientInput(e.target.value)}
+              />
+              <button
+                type="button"
+                onClick={addIngredient}
+                className="px-4 py-2 rounded-md bg-emerald-600 text-white hover:bg-emerald-700"
+              >
+                Add
+              </button>
             </div>
 
-
-            {/* Ingredients */}
-            <div className="mb-4">
-                <label className={formStyles.label}>Ingredients</label>
-
-                <div className="flex gap-2">
-                    <input
-                        className={formStyles.input}
-                        type="text"
-                        value={ingeredientInput}
-                        onChange={(e) => setIngredientInput(e.target.value)}
-                        />
-                    <button 
-                        type="button"
-                        onClick={addIngredient}
-                        className='px-4 py-2 rounded-md bg-emerald-600 text-white hover:bg-emerald-700'
-                        >
-                        Add
+            {ingredients.length > 0 && (
+              <ul className="mt-3 space-y-2">
+                {ingredients.map((ing, idx) => (
+                  <li
+                    key={idx}
+                    className="px-3 py-2 bg-gray-100 text-gray-900 rounded-md flex justify-between items-center"
+                  >
+                    <span>{ing}</span>
+                    <button
+                      type="button"
+                      onClick={() => removeIngredient(idx)}
+                      className="px-2 py-1 bg-red-600 text-white rounded hover:bg-red-700 text-sm"
+                    >
+                      Remove
                     </button>
-                    
-                </div>
+                  </li>
+                ))}
+              </ul>
+            )}
+          </div>
 
-                {ingredients.length > 0 && (
-                    <ul className="mt-3 space-y-2">
-                        {ingredients.map((ing, idx) => (
-                            <li
-                            key={idx}
-                            className="px-3 py-2 bg-gray-100 text-gray-900 rounded-md flex justify-between items-center"
-                            >
-                                <span>{ing}</span>
-                                <button
-                                    type="button"
-                                    onClick={() => removeIngredient(idx)}
-                                    className="px-2 py-1 bg-red-600 text-white rounded hover:bg-red-700 text-sm"
-                                >
-                                    Remove
-                                </button>
-                            </li>
-                        ))}
-                    </ul>
-                )}
-            </div>
-
-            {/* Cost*/}
-            <div className="mb-4">
-                <label className={formStyles.label}>Cost</label>
-                <input
-                  className={formStyles.input}
-                  type="number"
-                  min="0"
-                  placeholder="Enter cost"
-                  value={cost}
-                  onChange={(e) => setCost(e.target.value)}
-                />
-            </div>
+          {/* Cost*/}
+          <div className="mb-4">
+            <label className={formStyles.label}>Cost</label>
+            <input
+              className={formStyles.input}
+              type="number"
+              min="0"
+              placeholder="Enter cost"
+              value={cost}
+              onChange={(e) => setCost(e.target.value)}
+            />
+          </div>
 
           {/* Prep Steps */}
           <div className="mb-4">
@@ -146,7 +153,3 @@ return (
     </div>
   );
 }
-                        
-           
-            
-        
