32f342b4a7e36ee29ed2acb75e7f0ece4e846f99
ramithabet <ramithabet31@gmail.com>
2026-03-27 17:41:47 -0400

    Removed unused function

--- soaeco-soen341_project_w26/app/weekly_planner/actions.ts
+++ soaeco-soen341_project_w26/app/weekly_planner/actions.ts
@@ -18,15 +18,7 @@ import {
 
 // Change this function to fetch real calorie goals 
 // Once backend for weekly planner is implemented.
-export const getUserCalorieGoals = async (userId: string) => {
-    void userId;
 
-    return {
-        success: true,
-        dailyCalorieGoal: 2000,
-        weeklyCalorieGoal: 14000,
-    };
-};
 
 const getAuthenticatedPlannerUser = async (accessToken: string) => {
     if (!accessToken) {
